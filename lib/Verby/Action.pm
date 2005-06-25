375c40caa067090d7cb3f2e37d52ba3a2d862fe2
Stevan Little <stevan.little@iinteractive.com>
2005-06-25 16:34:49 +0000

    Documention

--- lib/Verby/Action.pm
+++ lib/Verby/Action.pm
@@ -37,12 +37,101 @@ __END__
 
 =head1 NAME
 
-Verby::Action - 
+Verby::Action - The baseclass for an action in Verby.
 
 =head1 SYNOPSIS
 
-	use Verby::Action;
+	use base qw/Verby::Action/;
+	sub do { ... }
+	sub confirm { ... }
 
 =head1 DESCRIPTION
+
+A Verby::Action is basically a reusable bit of code.
+
+Assuming it gets a L<Verby::Context> object sent to both C<do> and C<verify>,
+it knows to check whether it needs to be done, and actually do the job.
+
+Think of it as an abstraction of a make target.
+
+=head1 METHODS
+
+=over 4
+
+=item do $cxt
+
+The thing that the action really does. For example
+
+	package Verby::Action::Download;
+
+	sub do {
+		my ($self, $c) = @_;
+		system("wget", "-O", $c->file, $c->url);
+	}
+
+Will use wget to download C<< $c->url >> to C<< $c->file >>.
+
+This is a bad example though, you ought to subclass L<Verby::Action::RunCmd> if
+you want to run a command.
+
+=item verify $cxt
+
+Perform a boolean check - whether or not the action needs to happen or not.
+
+For example, if C<do> downloads C<< $c->file >> from C<< $c->url >>, then the
+verify method would look like:
+
+	sub verify {
+		my ($self, $c) = @_;
+		-f $c->file;
+	}
+
+=item confirm $cxt
+
+Typically called at the end of an action's do:
+
+	sub do {
+		my ($self, $c) = @_;
+		...
+		$self->confirm($c);
+	}
+
+It will call C<< $c->logger->logdie >> unless C<verify> returns a true value.
+
+=back
+
+=head1 ASYNCHRONEOUS INTERFACE
+
+An asynchroneous action typically implements two or three methods instead of
+C<do>, analogeous to C<IPC::Run>'s nonblocking interface:
+
+=over 4
+
+=item start $cxt
+
+Initiate the action, returning as early as possible.
+
+=item finish $cxt
+
+Clean up the action.
+
+=item pump $cxt
+
+Perform any nonblocking operation needed to keep things moving.
+
+If this retrurns a false value, the action is considered finished, and
+C<finish> will be called by the C<Verby::Dispatcher>.
+
+=back
+
+Note that this documentation assumes delegation of step methods to action
+methods.
+
+L<Verby::Dispatcher> actually has nothing to do with L<Verby::Action>, it's
+just that typically a L<Verby::Step> is just a thin wrapper for
+L<Verby::Action>, so the methods roughly correspond.
+
+See L<Verby::Step::Closure> for a trivial way to generate steps given a
+L<Verby::Action> subclass.
 
 =cut
--- lib/Verby/Step.pm
+++ lib/Verby/Step.pm
@@ -32,12 +32,87 @@ __END__
 
 =head1 NAME
 
-Verby::Step - 
+Verby::Step - A base class representing a single thing to be executed by
+L<Verby::Dispatcher>.
 
 =head1 SYNOPSIS
 
-	use Verby::Step;
+	use base qw/Verby::Step/;
+
+Or perhaps more appropriate
+
+	use Verby::Step::Closure qw/step/;
+	my $step = step "Some::Action",
+		sub { warn "before" },
+		sub { warn "after" };
 
 =head1 DESCRIPTION
+
+A step in the L<Verby> system is like an instance of an action.
+
+A step is much like a makefile target. It can depend on other steps, and when
+appropriate will be told to be executed.
+
+The difference between a L<Verby::Step> and a L<Verby::Action> is that an
+action is usually just reusable code to implement the verification and
+execution
+
+A step manages the invocation of an action, typically by massaging the context
+before delegating, and re-exporting meaningful data to the parent context when
+finished. It also tells the system when to execute, by specifying dependencies.
+
+The distinction is that an action is something you do, and a step is something
+you do before and after others.
+
+=head1 METHODS
+
+All methods should be subclassed except for C<provides_cxt>. This class is
+nearly completely virtual.
+
+=over 4
+
+=item depends
+
+Subclass this to return a list of other steps to depend on.
+
+=item is_satisfied
+
+This method should return a true value if the step does not need to be
+executed.
+
+Typically a delegation to L<Verby::Action/verify>. They are named differently,
+because C<is_satisfied> implies state. The L<Verby::Dispatcher> will sometimes
+make assumptions, without asking the step to check that it is satisfied.
+
+=item do
+
+=item start
+
+=item finish
+
+=item pump
+
+These are basically delegations to the corresponding L<Verby::Action> methods.
+
+The only interesting thing to do here is to fudge the context up a bit. For
+example, if your action assumes the C<path> key to be in the context, but you
+chose C<the_path_to_the_thing> to be in your config, this is the place to do:
+
+	sub do {
+		my ($self, $c) = @_;
+
+		# prepare for the action
+		$c->path($c->the_path_to_the_thing);
+
+		$self->action->do($c);
+
+		# pass data from the action to the next steps
+		$c->export("some_key_the_action_set");
+	}
+
+L<Verby::Step::Closure> provides a convenient way to get this behavior for
+free.
+
+=back
 
 =cut
