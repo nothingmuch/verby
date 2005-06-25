8f4b6f3f610e7da5411ff0d0f2ff5a473d7e521b
Stevan Little <stevan.little@iinteractive.com>
2005-06-25 16:35:03 +0000

    implementation plus clumsy doc

--- lib/Config/Source/ARGV.pm
+++ lib/Config/Source/ARGV.pm
@@ -1,11 +1,21 @@
 #!/usr/bin/perl
 
 package Config::Source::ARGV;
-use base qw//;
-use fields qw//;
+use base qw/Config::Data/;
 
 use strict;
 use warnings;
+
+use Getopt::Casual;
+
+sub new {
+	my $self = shift->SUPER::new(@_);
+
+	%{ $self->{data} } = map {
+		(my $key = $_) =~ s/^-+//; # Getopt::Casual exposes '--foo', etc.
+		$key => $ARGV{$_};
+	} keys %ARGV;
+}
 
 __PACKAGE__
 
@@ -15,12 +25,29 @@ __END__
 
 =head1 NAME
 
-Config::Source::ARGV - 
+Config::Source::ARGV - L<Config::Data> fields from the command line
 
 =head1 SYNOPSIS
 
 	use Config::Source::ARGV;
 
+	my $argv = Config::Source::ARGV->new
+	my $config_hub = Config::Data->new($argv, $other_source);
+
+Use a field
+
+	sub do {
+		my ($self, $c) = @_;
+		print $c->handbag;
+	}
+
+And then on the command line, set it:
+
+	my_app.pl --handbag=gucci
+
 =head1 DESCRIPTION
+
+This module is useful for getting some global keys set or perhaps overridden on
+the command line.
 
 =cut
