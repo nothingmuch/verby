b0c8d0866115e82bc3cef10978df45f198f42720
Stevan Little <stevan.little@iinteractive.com>
2005-06-25 16:34:56 +0000

    Documentation

--- lib/Verby/Context.pm
+++ lib/Verby/Context.pm
@@ -34,12 +34,40 @@ __END__
 
 =head1 NAME
 
-Verby::Context - 
+Verby::Context - A sort of scratchpad every L<Verby::Step> gets from
+L<Verby::Dispatcher>.
 
 =head1 SYNOPSIS
 
-	use Verby::Context;
+	sub do {
+		my $self = shift;
+		my $context = shift;
+
+		print $context->rockets; # get a value
+		$context->milk("very"); # set a value
+	}
 
 =head1 DESCRIPTION
+
+A context has two roles in L<Verby>. The first is to control what a
+L<Verby::Action> will do, by providing it with parameters, and the other is to
+share variables that the action sets, so that other steps may have them too.
+
+It is a mutable L<Config::Data> that derives from the global context.
+
+=head1 EXAMPLE USAGE
+
+See the annotated F<scripts/module_builder.pl> for how a context is used in
+practice.
+
+=head1 THE LOGGER FIELD
+
+	$c->logger;
+
+will delegate to L<Log::Log4perl/get_logger>, sending it a nice string for a
+category.
+
+The category is the class of the caller, concatenated a stringification of the
+object if the object can stringify.
 
 =cut
--- scripts/module_builder.pl
+++ scripts/module_builder.pl
@@ -45,28 +45,22 @@ foreach my $tarball (@ARGV){
 	}, sub {
 		my $self = shift;
 		my $c = shift;
-		$c->export("src_dir") if $c->exists("src_dir");
+		if ($c->exists("src_dir")) {
+			$c->workdir($c->src_dir);
+			$c->export("workdir");
+		}
 	};
 	$untar->depends($mkdir);
 
-	my $plscript = step "Verby::Action::MakefilePL" => sub {
-		my $self = shift;
-		my $c = shift;
-		$c->workdir($c->src_dir);
-	};
+	my $plscript = step "Verby::Action::MakefilePL";
 	$plscript->depends($untar);
 
-	my $make = step "Verby::Action::Make" => sub {
-		my $self = shift;
-		my $c = shift;
-		$c->workdir($c->src_dir);
-	};
+	my $make = step "Verby::Action::Make";
 	$make->depends($plscript);
 
 	my $test = step "Verby::Action::Make" => sub {
 		my $self = shift;
 		my $c = shift;
-		$c->workdir($c->src_dir);
 		$c->target("test");
 	};
 	$test->depends($make);
@@ -76,3 +70,69 @@ foreach my $tarball (@ARGV){
 
 $d->do_all;
 
+__END__
+
+=pod
+
+=head1 NAME
+
+module_builder.pl - A simple demo that unpacks, builds and tests perl module
+tarballs.
+
+=head1 SYNOPSIS
+
+	$ module_builder.pl My-Module-0.01.tar.gz Verby-0.01.tar.gz
+
+=head1 DESCRIPTION
+
+As this is not really a useful program, we'll look at how it's written, not how
+it's used.
+
+Foreach tarball given on the command line, several steps are created, all of
+them L<Verby::Step::Closure> objects. Each step depends on the step before it.
+
+=over 4
+
+=item L<Verby::Action::MkPath>
+
+First the context variable C<untar_dir> is created. This is where all the
+tarballs will be unpacked to. Because we're so silly, C<untar_dir> is actually
+set by L<Cwd/cwd>, so this is basically a no-op.
+
+It has C<provide_cxt>, meaning that all it's dependant steps and it share a
+common L<Verby::Context>, separate from other steps.
+
+=item L<Verby::Action::Untar>
+
+The next step is to unpack the tarball. The C<before> handler in
+L<Verby::Step::Closure> will set the C<tarball> context variable to the
+argument being processed, and the C<dest> context variable to the C<untar_dir>
+context variable.
+
+The C<after> handler sets the C<workdir> variable to the value of the
+C<src_dir> variable, and exports C<workdir> so that it's accessible to
+subsequent steps.
+
+=item L<Verby::Action::MakefilePL>
+
+The next thing to do is run the C<Makefile.PL> file. We set C<workdir> to
+C<src_dir> in the previous step, and that's all we need.
+
+=item L<Verby::Action::Make>
+
+After we've finished running C<Makefile.PL> we can run make(1) with no
+arguments. This is exactly what this step does. It is also affected by the
+previously exported C<workdir>.
+
+=item L<Verby::Action::Make>
+
+Now we run make(1) again, but this time with the C<test> target.
+
+=back
+
+=head1 ASYNC BEHAVIOR
+
+Since most actions are asynchroneous, modules will be built in parallel. Isn't
+that cool?
+
+=cut
