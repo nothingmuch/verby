18857e459df20dde9887fca36c80641970b313f1
Stevan Little <stevan.little@iinteractive.com>
2005-06-25 16:33:51 +0000

    just adding the XML stuff, it will move/change

--- lib/Config/Source/XML.pm
+++ lib/Config/Source/XML.pm
@@ -1,11 +1,64 @@
 #!/usr/bin/perl
 
 package Config::Source::XML;
-use base qw//;
-use fields qw//;
 
 use strict;
 use warnings;
+
+use $VERSION = '0.01';
+
+use XML::SAX::ParserFactory;
+
+sub new {
+    my $class = shift;
+    return bless { _config => undef }, ref($class) || $class;
+}
+
+sub load {
+    my ($self, $file) = @_;
+    (-e $file && -f $file)
+        || die "Bad config file '$file' either it doesn't exist or it's not a file";    
+    my $handler = Config::Source::XML::SAX::Handler->new();
+    my $p = XML::SAX::ParserFactory->parser(Handler => $handler);
+    $p->parse_uri($file);
+    $self->{_config} = $handler->config();
+}
+
+package Config::Source::XML::SAX::Handler;
+
+use strict;
+use warnings;
+
+use $VERSION = '0.01';
+
+use base 'XML::SAX::Base';
+
+sub new {
+    my $class = shift;
+    my $self = $class->SUPER::new(@_);
+    $self->{_config} = undef;
+    return $self;
+}
+
+sub config { (shift)->{_config} }
+
+sub start_element {
+    my ($self, $el) = @_;
+    my $tag_name = lc($el->{Name});
+    if ($tag_name eq 'installer') {
+        $self->{_config} = {} unless $self->{_config};    
+    }
+    else {
+        die "I dont recognize the tag name '$tag_name'";
+    }
+}
+
+sub _get_value {
+    my ($self, $el, $key) = @_;
+    return undef unless exists $el->{Attributes}->{'{}' . $key};
+    return $el->{Attributes}->{'{}' . $key}->{Value};        
+}
+
 
 __PACKAGE__
 
