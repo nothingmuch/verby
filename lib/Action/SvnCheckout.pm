63dfccf29b726a5f9e4ddd8f0c843c7c05d8a82c
Stevan Little <stevan.little@iinteractive.com>
2005-06-25 16:32:24 +0000

    Modify to use Action::RunCmd

--- lib/Action/SvnCheckout.pm
+++ lib/Action/SvnCheckout.pm
@@ -2,27 +2,19 @@
 
 
 package Action::SvnCheckout;
-use base qw/Action/;
+use base qw/Action::RunCmd/;
 
 use strict;
 use warnings;
 
-use IPC::Run qw/run/;
-use Fatal qw/run/;
 use File::Spec;
 
 sub do {
 	my $self = shift;
 	my $c = shift;
 
-	my $from = $c->source;
-	my $to = $c->dest;
-	my $svn = $c->svn_exe;
+	$self->run($c, ["svn", $c->source, $c->dest]);
 
-	run [$svn, $from, $to], \(my ($in, $out, $err));
-
-	$c->error($err);
-	
 	$self->confirm;
 }
 
@@ -30,7 +22,11 @@ sub verify {
 	my $self = shift;
 	my $c = shift;
 
-	return unless -d $c->dest;
+	my $wd = $c->dest;
+
+	return unless -d $wd
+		and -d File::Spec->catdir($wd, ".svn")
+		and $self->run($c, [qw/svn up/], undef, sub { chdir $wd };
 }
 
 __PACKAGE__
